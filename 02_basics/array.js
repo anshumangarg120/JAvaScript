ebec65411e9a3fd985f7938560123a0397048f7f
Anshuman Garg <anshumangarg120@gmail.com>
2024-01-02 18:52:31 +0530

    object1

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,5 +1,15 @@
-const myArr = [0, 1, 2, 3, 4, 5, 6, ]
-const myHeors = ["shaktimann", "zero"]
+// const myArr = [0, 1, 2, 3, 4, 5, 6, ]
+// const myHeors = ["shaktimann", "zero"]
 
-const mArr2 = new Array ()
-console.log(myArr[0]);
+// const mArr2 = new Array ()
+// console.log(myArr[0]);
+
+const hero = ["thor", "ironman", "spiderman"]
+const dc = ["superman", "flash", "batman"]
+
+// hero.push(dc)
+// console.log(hero);
+
+
+const allHero = hero.concat(dc)
+console.log(hero);
---
+++ 02_basics/object.js
@@ -0,0 +1,14 @@
+// singleton
+// object literals
+
+const jsUser = {
+    name: "Anish",
+    age: 23,
+    location: "Bhagalpur",
+    email: "anish@gmail.com",
+    isLoggedIn: false,
+    lastLoggedInn: ["Monday", "Sunday"],
+    
+}
+
+console.log(jsUser);
