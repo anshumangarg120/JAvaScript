8ab45552729c1e640a5d7cdd7fa5a32e846b0b44
Anshuman Garg <anshumangarg120@gmail.com>
2024-01-02 17:23:09 +0530

    time

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -1,4 +1,7 @@
-let myDate = new Date()
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleString());
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
