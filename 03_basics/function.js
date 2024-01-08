7c58887220aaba04e3d56dab5a7a35ee9a81af4e
Anshuman Garg <anshumangarg120@gmail.com>
2024-01-08 16:22:17 +0530

    study about function

---
+++ 03_basics/function.js
@@ -0,0 +1,36 @@
+
+
+function myName (){
+    console.log("A");
+    console.log("N");
+    console.log("I");
+    
+}
+
+// myName()
+
+// function addTwoNumbers (number1, number2){
+// console.log(number1 + number2)
+// }
+
+function addTwoNumbers (number1, number2){
+    // let result =  number1 + number2
+    // return result
+
+    return number1 + number2
+    }
+
+const result = addTwoNumbers(100,500)
+
+// console.log("hh", result);
+
+
+function loginUserMessage (username){
+    if (username === undefined){
+        console.log("please enter your username");
+        return 
+    }
+    return `${username} logged in`
+}
+
+console.log(loginUserMessage( ));
