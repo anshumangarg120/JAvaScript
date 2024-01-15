7951ff6bb1ca67376c1e8d8b89ffc1125fa3591d
Anshuman Garg <anshumangarg120@gmail.com>
2024-01-16 00:23:01 +0530

    study about add item through map in react

--- reactsheryians/reacts/src/App.jsx
+++ reactsheryians/reacts/src/App.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
 
 function App() {
- const [val, setVal] = useState([1,2,3,4,5,6]);
+ const [val, setVal] = useState([
+  {name: "Anish", age: 24}, 
+  {name: "Sivam", age: 25}, 
+  {name: "manish", age: 32},
+]);
 
   return (
-   <div className='p-10'>
-    {val.map(item => <h1>{item}</h1> )}
-    <button onClick={()  => setVal( () => {
-      return val.filter((item, index) => index!=val.length-1 )
-    }) } className="rounded-full px-5 py-1 bg-blue-600  text-white">-</button>
-   </div>
+    <div className='p-10 font-bold'>
+      {val.map(item => (
+        <div>
+      <h1>{item.name}</h1>
+      <h1>{item.age}</h1>
+      </div>
+      ))}
+      <button 
+       onClick={()=>setVal (() => val.map(item => item.name === "Anish" ? ({name: "Anish", age: 100 }) : item )) }
+       className="rounded-full px-5 py-1 bg-blue-600  text-white"
+      >
+         click
+      </button>
+    </div>
   )
 }
 
