911d89571104de046f62933043b8b4f79b02ed42
Anshuman Garg <anshumangarg120@gmail.com>
2024-01-18 23:28:29 +0530

    section 9 complited

--- reactsheryians/reacts/src/components/Card.jsx
+++ reactsheryians/reacts/src/components/Card.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-function Card() {
+function Card({data, handleClick, index}) {
+
+  const {image, artist, name, added} = data;
+ 
   return (
-   <div>Card</div>
+    <>
+     <div className="max-w-30 rounded overflow-hidden shadow-lg bg-white mt-10">
+      <img src={image} alt="Music Image" className="w-full h-40 object-cover bg-orange-200"/>
+     <div className="px-3 py-2">
+      <div className="font-bold text-md mb-2 text-orange-500">{artist}</div>
+       <p className="text-gray-700 text-base mb-4">
+        {name}
+       </p>
+        <button onClick={() => handleClick (index)} className={`  ${added === false ? "bg-orange-500" : "bg-green-500"}  text-white font-bold py-1 px-2 rounded`}>
+           {added === false ? "Add To Favourites" : "Added"}
+        </button>
+      </div>
+     </div>
+</>
   )
 }
 
--- reactsheryians/reacts/src/components/Navbar.jsx
+++ reactsheryians/reacts/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function Navbar() {
+function Navbar({data}) {
   return (
-    <div className='w-full px-4 py-3 flex justify-between items-center'>Navbar</div>
+    <div className='w-full px-20 py-3 flex justify-between items-center'>
+      <h3>Orange</h3>
+      <div className='flex p-2 bg-orange-600 text-white px-4 roounded-md text-sm gap-3 '>
+        <h3>Favourite</h3>
+        <h4>{data.filter(item => item.added).length}</h4>
+      </div>
+    </div>
   )
 }
 
